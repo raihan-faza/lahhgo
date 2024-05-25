d9ad3f9e6c2dd3dfdda2150abf6cc8ba728344de
raihan-faza <raihanfazasabiqr@gmail.com>
2024-05-25 20:12:04 +0700

    adding transactions, invoice database models and alter sampah status choices

--- sampah/models.py
+++ sampah/models.py
@@ -9,10 +9,23 @@ class Customer(models.Model):
 
 class Sampah(models.Model):
     tags = {"dry": "kering", "wet": "basah"}
-    stats = {"WTG": "WAITING", "ACC": "ACCEPTED"}
+    stats = {"AVL": "AVAILABLE", "SLD": "SOLD"}
     user = models.ForeignKey(Customer, on_delete=models.CASCADE)
     jenis = models.CharField(max_length=200)
     jumlah = models.IntegerField()
     deskripsi = models.CharField(max_length=200)
     tag = models.CharField(max_length=6, choices=tags)
     status = models.CharField(max_length=8, choices=stats)
+
+
+class Invoice(models.Model):
+    total_harga = models.IntegerField()
+    alamat_asal = models.CharField(max_length=200)
+    alamat_tujuan = models.CharField(max_length=200)
+
+
+class Transactions(models.Model):
+    transaction_invoice = models.OneToOneField(
+        Invoice, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    sampah = models.OneToOneField(Sampah, on_delete=models.CASCADE)
